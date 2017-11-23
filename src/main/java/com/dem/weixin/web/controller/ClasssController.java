05f22e15e3d4657acfeb797d283f5e8892fc5718
dem <dem@gmail.com>
2017-11-23 23:01:31 +0800

    update

--- src/main/java/com/dem/weixin/web/controller/ClasssController.java
+++ src/main/java/com/dem/weixin/web/controller/ClasssController.java
@@ -2,6 +2,7 @@ package com.dem.weixin.web.controller;
 
 import com.dem.weixin.domain.TbClass;
 import com.dem.weixin.service.ClassService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Created by DEM
  */
 @Controller
+@Api("教室控制器")
 public class ClasssController {
     @Autowired
     private ClassService classService;
