dc41c51402c9ba7a09e705cd08ba78e91b185885
dem <dem@gmail.com>
2017-11-23 23:03:01 +0800

    add swagger-ui

---
+++ src/main/java/com/dem/weixin/conf/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.dem.weixin.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableWebMvc
+@EnableSwagger2
+@ComponentScan(basePackages = {"com.dem.weixin.web.controller"})
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.dem.weixin.web.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring 中使用Swagger2构建RESTful APIs")
+                .termsOfServiceUrl("www.baidu.com")
+                .contact("Mr.p")
+                .version("1.0")
+                .build();
+    }
+}
